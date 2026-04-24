2deffd4b82b66c13eaf19b522acf71ed882e41c3
navaneetha1201-a11y <navaneetha1201@gmail.com>
2026-04-24 16:48:27 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,28 +1,24 @@
-import { Post } from "@/interfaces/post";
-import fs from "fs";
-import matter from "gray-matter";
-import { join } from "path";
+import { GraphQLClient, gql } from "graphql-request";
 
-const postsDirectory = join(process.cwd(), "_posts");
+const client = new GraphQLClient("https://gql.hashnode.com");
 
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
-}
+export async function getPosts() {
+  const query = gql`
+    query {
+      publication(host: "navaneetha.in") {
+        posts(first: 10) {
+          edges {
+            node {
+              title
+              slug
+              brief
+            }
+          }
+        }
+      }
+    }
+  `;
 
-export function getPostBySlug(slug: string) {
-  const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  const { data, content } = matter(fileContents);
-
-  return { ...data, slug: realSlug, content } as Post;
-}
-
-export function getAllPosts(): Post[] {
-  const slugs = getPostSlugs();
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+  const data = await client.request(query);
+  return data.publication.posts.edges;
 }
